55a9bc1f916fb02eca5e59a1bdc7acc307297b87
Shahad161 <shahad.hamza.014@gmail.com>
2021-11-30 11:46:28 +0300

    add the main packages

--- app/src/main/java/com/example/marvel/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/marvel/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.marvel
+package com.example.marvel.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.marvel.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
