f61505f9f61f8ef701e35350cc3678a4da19939d
Shahad161 <shahad.hamza.014@gmail.com>
2021-12-02 10:52:08 +0300

    fix: navigation error

--- app/src/main/java/com/example/marvel/ui/MainActivity.kt
+++ app/src/main/java/com/example/marvel/ui/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.marvel.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.setupWithNavController
+import com.example.marvel.R
 import com.example.marvel.data.local.MarvelDataBase
 import com.example.marvel.databinding.ActivityMainBinding
-import com.example.marvel.R
-import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -14,25 +18,22 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        binding= DataBindingUtil.setContentView<ActivityMainBinding>(
-            this,
-            R.layout.activity_main
-        ).also { binding ->
-            binding.lifecycleOwner=this
-        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
         MarvelDataBase.init(applicationContext)
+
     }
 
+    override fun onResume() {
+        super.onResume()
+        binding.navigation
+            .setupWithNavController(findNavController(R.id.fragment_host))
+    }
 
-//    override fun onResume() {
-//        super.onResume()
-//        binding.navigation
-//            .setupWithNavController(findNavController(R.id.fragment_host))
-//    }
-//
-//    override fun onSupportNavigateUp(): Boolean {
-//        findNavController(R.id.fragment_host).navigateUp()
-//        return true
-//    }
+    override fun onSupportNavigateUp(): Boolean {
+        findNavController(R.id.fragment_host).navigateUp()
+        return true
+    }
+
 }
